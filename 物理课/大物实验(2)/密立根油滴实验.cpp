7b662aea9e50437085d1752ffbd3012922887e52
Hytidel <124644694+Hytidel@users.noreply.github.com>
2023-09-25 16:29:47 +0800

    Add files via upload

---
+++ 物理课/大物实验(2)/密立根油滴实验.cpp
@@ -0,0 +1,164 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstdlib>
+#include<iomanip>
+#include<cmath>
+#include<cstring>
+#include<algorithm>
+#include<complex>
+#include<array>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<set>
+#include<map>
+#include<list>
+#include<unordered_set>
+#include<unordered_map>
+#include<bitset>
+#include<valarray>
+#include<sstream>
+#include<functional>
+#include<cassert>
+#include<random>
+#include<numeric>
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef queue<int> qi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef tuple<int, int, int> tiii;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef queue<pii> qii;
+typedef complex<double> cp;
+#define umap unordered_map
+#define uset unordered_set
+#define pque priority_queue
+#define IOS ios::sync_with_stdio(0); cin.tie(0), cout.tie(0);
+#define CaseT int CaseT; cin >> CaseT; while(CaseT--)
+#define endl '\n'
+#define npt nullptr
+#define so sizeof
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define hashset_finetune(p) (p).reserve(1024); (p).max_load_factor(0.25);  // 哈希表防卡
+const double eps = 1e-7;
+const double pi = acos(-1.0);
+const int INF = 0x3f3f3f3f;
+const ll INFF = 0x3f3f3f3f3f3f3f3f;
+// const int dx[4] = { -1,0,1,0 }, dy[4] = { 0,1,0,-1 };
+// ----------------------------------------------------------------
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
+ll lowbit(ll x) { return x & (-x); }
+int cmp(double a, double b) {
+    if (fabs(a - b) < eps) return 0;
+    else if (a > b) return 1;
+    else return -1;
+}
+int sgn(double x) {
+    if (fabs(x) < eps) return 0;
+    else if (x > 0) return 1;
+    else return -1;
+}
+
+template<typename T>
+void read(T& x) {
+    x = 0;
+    T sgn = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') sgn = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch & 15);
+        ch = getchar();
+    }
+    x *= sgn;
+}
+
+template<typename T>
+void write(T x) {
+    if (x < 0) {
+        putchar('-');
+        x *= -1;
+    }
+    if (x < 10) putchar(x + 48);
+    else write(x / 10), putchar(x % 10 + 48);
+}
+
+ll qpow(ll a, ll k, ll MOD) {
+    ll res = 1;
+    while (k) {
+        if (k & 1) res = res * a % MOD;
+        k >>= 1;
+        a = a * a % MOD;
+    }
+    return res;
+}
+
+ll qpow(ll a, ll k) {  // 注意可能爆ll
+    ll res = 1;
+    while (k) {
+        if (k & 1) res = res * a;
+        k >>= 1;
+        a *= a;
+    }
+    return res;
+}
+// ----------------------------------------------------------------
+
+string get(double n) {
+    int cnt = 0;
+    while ((int)n == 0) n *= 10, cnt++;
+    return to_string(n) + "e-" + to_string(cnt);
+}
+
+void solve() {
+    //double tg, u; cin >> tg >> u;
+
+    //double a = sqrt((6.0 * 1.83 * pow(10, -5) * 6 * 0.25 * pow(10, -3)) / (2 * 0.93 * pow(10, 3) * 9.8 * tg));
+    //double q = (1.83 * pow(10, -5) * 6 * 0.25 * pow(10, -3)) / (tg * (1 + (6.17 * pow(10, -6) / (76 * a))));
+    //q = pow(q, (double)3 / 2);
+    //q *= (18 * pi) / sqrt(2 * 0.93 * pow(10, 3) * 9.8);
+    //q *= 5 * pow(10, -3) / u;
+    //cout << "a = " << get(a) << endl;
+    //cout << "q = " << get(q) << endl;
+
+    //double n0 = q / (1.602 * pow(10, -19));
+    //int n = (int)n0;  // 忘记四舍五入
+    //cout << "n0 = " << n0 << endl;
+    //cout << "n = " << n << endl;
+    //cout << "e = " << q / n << endl;
+    //cout << endl;
+
+    cout << sqrt((pow(1.359 - 1.652, 2) + pow(1.741 - 1.652, 2) + pow(2.262 - 1.652, 2)
+        + pow(1.446 - 1.652, 2) + pow(1.678 - 1.652, 2) + pow(1.424 - 1.652, 2)) / 30);
+}
+
+int main() {
+    IOS;
+#ifndef ONLINE_JUDGE
+    clock_t my_clock = clock();
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    // ----------------------------------------------------------------	
+    // init();
+    // CaseT  // 单测时注释掉该行
+    solve();
+    // ----------------------------------------------------------------
+#ifndef ONLINE_JUDGE
+    cout << endl << "Time used " << clock() - my_clock << " ms." << endl;
+#endif
+    return 0;
+}
