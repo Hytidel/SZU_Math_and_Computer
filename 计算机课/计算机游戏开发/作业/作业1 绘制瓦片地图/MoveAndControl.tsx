a323c1b5ec7eca0f1a3d03dc35efe6e9c5a72e92
Hytidel <124644694+Hytidel@users.noreply.github.com>
2024-07-12 07:00:12 +0800

    Add files via upload

---
+++ 计算机课/计算机游戏开发/作业/作业1 绘制瓦片地图/MoveAndControl.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="MoveAndControl" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="228" columns="19">
+ <image source="../../实验/Chapter05-Chapter12/可执行文件/Chapter05/MoveAndControl.png" width="640" height="400"/>
+</tileset>
---
+++ 计算机课/计算机游戏开发/作业/作业1 绘制瓦片地图/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="地图素材/6.png" width="512" height="512"/>
+</tileset>
